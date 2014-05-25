0cc7fbf308fa172884143c01497b4d849b7d62ef
Justin Tanacredi <Jtanacredi@gmail.com>
2014-05-25 14:03:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surveyfy::Application.config.secret_key_base = '20397138617854daf3d4adcbed5066574ce79da945ae1cf217be2cc9ec0384cd90c637fd790082351835b5e2c66731c8134882f020c9002d72a831698dbd11d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Surveyfy::Application.config.session_store :cookie_store, key: '_surveyfy_session'
